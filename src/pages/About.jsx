aaaeee074bbb5644393fb31472a5edb879a6cef7
Soumyadeep <soumyadip243@gmail.com>
2023-05-28 20:47:01 +0530

    created about page

---
+++ src/pages/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <section className='hero-section' id='about'>
+        <div className="container custom-width">
+            <div className="main-content">
+                <div className="section-header">
+                    <h4 className='subtitle__rounded'><i className="far fa-user"></i> about</h4>
+                    <h1>Hello, I'm <span className="name__highlight">Soumyadeep Saha</span></h1>
+                </div>
+                <p className="intro-text">Looking forward to learn new web technologies and code the world of my imagination</p>
+            </div>
+        </div>
+    </section>
+  )
+}
