ebadec36e7e19a3b673a0bae33a651ca52fa21b9
Soumyadeep <soumyadip243@gmail.com>
2023-05-28 20:46:31 +0530

    added active state

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,30 +6,38 @@ export default function Navbar() {
 
   const [hoverActiveLink, setHoverActiveLink] = useState('Home')
 
+  const [activeLink, setActiveLink] = useState('Home');
+
   const navLinks = [
     {
       label : 'Home', 
-      icon: 'fa fa-home'
+      icon: 'fa fa-home',
+      to: '#home'
     }, 
     {
       label : 'About', 
-      icon: 'far fa-user'
+      icon: 'far fa-user',
+      to: '#about'
     }, 
     {
       label : 'Skills', 
-      icon: 'fa fa-tasks'
+      icon: 'fa fa-tasks',
+      to: '#home'
     }, 
     {
       label : 'Projects', 
-      icon: 'fa fa-code-branch'
+      icon: 'fa fa-code-branch',
+      to: '#home'
     }, 
     {
       label : 'Education', 
-      icon: 'fa fa-user-graduate'
+      icon: 'fa fa-user-graduate',
+      to: '#home'
     }, 
     {
       label : 'Contact', 
-      icon: 'far fa-envelope'
+      icon: 'far fa-envelope',
+      to: '#home'
     }, 
   ]
 
@@ -37,7 +45,7 @@ export default function Navbar() {
     <div className='navbar-right'>
       <ul className="navigation list-group d-flex">
         {navLinks.map((item, index) => {
-          return <li key={index} className="nav-item list-group-item"><a href="/" onMouseEnter={(e) => {setTooltip(true); setHoverActiveLink(item.label)}} onMouseLeave={() => setTooltip(false)}><span className={(hoverActiveLink === item.label && tooltip) ? 'tooltip-text visible' : 'tooltip-text'} >{item.label}</span><i className={item.icon}></i></a></li>
+          return <li key={index} className="nav-item list-group-item"><a href={item.to} className={activeLink == item.label ? 'active' : ''} onClick={() => setActiveLink(item.label)}  onMouseEnter={(e) => {setTooltip(true); setHoverActiveLink(item.label)}} onMouseLeave={() => setTooltip(false)}><span className={(hoverActiveLink === item.label && tooltip) ? 'tooltip-text visible' : 'tooltip-text'} >{item.label}</span><i className={item.icon}></i></a></li>
         })}
       </ul>
     </div>
