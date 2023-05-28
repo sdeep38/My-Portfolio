f336b14171ae63f7d8fe6ce18abd2cd9d3087b6d
Soumyadeep <soumyadip243@gmail.com>
2023-05-28 20:47:28 +0530

    created home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <section className='hero-section' id='home'>
+        <div className="container custom-width">
+            <div className="main-content">
+                <div className="section-header">
+                    <h4 className='subtitle__rounded'><i className="fa fa-home"></i> introduce</h4>
+                    <h1>Hello, I'm <span className="name__highlight">Soumyadeep Saha</span></h1>
+                </div>
+                <p className="intro-text">Looking forward to learn new web technologies and code the world of my imagination</p>
+            </div>
+        </div>
+    </section>
+  )
+}
