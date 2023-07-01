dd66c1aa86f004995927af6134f94d49dd6cdd60
Soumyadeep <soumyadip243@gmail.com>
2023-07-01 22:58:45 +0530

    added new component routes

--- src/App.js
+++ src/App.js
@@ -3,15 +3,27 @@ import Navbar from './components/Navbar';
 import LeftSidebar from './components/LeftSidebar';
 import Home from './pages/Home';
 import About from './pages/About';
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
+import HamburgerMenu from './components/HamburgerMenu';
+import { useState } from 'react';
+import Skills from './pages/Skills';
+import Education from './pages/Education';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <HamburgerMenu />
+      <Navbar showTooltip = {true} addClass={''}/>
       <LeftSidebar />
       <main className='wrapper'>
+        <LeftSidebar />
         <Home />
         <About />
+        <Skills />
+        <Projects />
+        <Education />
+        <Contact />
       </main>
     </div>
   );
