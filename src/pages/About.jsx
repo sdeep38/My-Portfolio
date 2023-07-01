9fa859746c412d9b51ce68d5d7b3f016ad68628d
Soumyadeep <soumyadip243@gmail.com>
2023-07-01 23:02:11 +0530

    updated intro text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 
 export default function About() {
+
   return (
-    <section className='hero-section' id='about'>
-        <div className="container custom-width">
-            <div className="main-content">
-                <div className="section-header">
-                    <h4 className='subtitle__rounded'><i className="far fa-user"></i> about</h4>
-                    <h1>Hello, I'm <span className="name__highlight">Soumyadeep Saha</span></h1>
-                </div>
-                <p className="intro-text">Looking forward to learn new web technologies and code the world of my imagination</p>
-            </div>
+    <section className='about-section' id='about'>
+      <div className="custom-container">
+        <div className="main-content custom-width">
+
+          <div className="section-header">
+            <h4 className='subtitle__rounded'><i className="far fa-user"></i> about</h4>
+            <h1>Undergraduate student at <span className="name__highlight">IIT Kharagpur</span></h1>
+          </div>
+
+          <p className="intro-text">Currently, a third year undergraduate pursuing my B-Tech major in Electrical Engineering from IIT Kharagpur, 
+          with specialization in Instrumentation Engineering. 
+          <br />Presently exploring the web development domain, inclined more towards backend development.</p>
+          
         </div>
-    </section>
+      </div>
+    </section >
   )
 }
