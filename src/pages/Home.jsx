830f1381a7e5360a1fbc783a5ad3c247c96bc6fb
Soumyadeep <soumyadip243@gmail.com>
2023-07-01 23:04:22 +0530

    modified class of main content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,16 @@ import React from 'react'
 export default function Home() {
   return (
     <section className='hero-section' id='home'>
-        <div className="container custom-width">
-            <div className="main-content">
-                <div className="section-header">
-                    <h4 className='subtitle__rounded'><i className="fa fa-home"></i> introduce</h4>
-                    <h1>Hello, I'm <span className="name__highlight">Soumyadeep Saha</span></h1>
-                </div>
-                <p className="intro-text">Looking forward to learn new web technologies and code the world of my imagination</p>
-            </div>
+      <div className="custom-container">
+        <div className="main-content custom-width">
+          <div className="section-header">
+            <h4 className='subtitle__rounded'><i className="fa fa-home"></i> introduce</h4>
+            <h1>Hello, I'm <span className="name__highlight">Soumyadeep Saha</span></h1>
+          </div>
+          <p className="intro-text">Welcome to my Portfolio website !</p>
+          <a href="#projects" className="go-to-projects fw_500 d-flex align-items-center justify-content-center"> <span>My Projects</span><i className="fa fa-arrow-down"></i> </a>
         </div>
+      </div>
     </section>
   )
 }
